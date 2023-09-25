f52dc5233b23a1afe029db0e91aa8e832a7cdb4d
Sionne.Quintero <94871596+BySAQQ@users.noreply.github.com>
2023-09-25 13:42:24 -0500

    Feat: Actvity N2

---
+++ practicaJS/Actividad2.js
@@ -0,0 +1,16 @@
+let myNumber   = 5;
+let myString   = "Hello World";
+let myBoolean  = true;
+let myArray    = [1, 2, 3, 4, 5];
+let myObject   = {name: "Sionne", surname: "Quintero", age: 23};
+let myUndefined;
+let myNull     = null;
+
+console.log(myNumber + " es de tipo " + typeof myNumber);
+
+console.log(myString + " es de tipo " + typeof myString);
+
+console.log(myNumber + myString);
+
+console.log(myObject.name + " " + myObject.surname + " tiene " + myObject.age + " años");
+
